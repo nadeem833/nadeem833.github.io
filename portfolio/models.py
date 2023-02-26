fa5f7044d2bb75573c7816c0b4804a9e9850c317
Muhammad Nadeem <bsit833@gmail.com>
2023-02-26 16:55:16 +0500

    uodate model for contac

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contact (models.Model):
-    name=models.name = models.CharField(max_length=25) 
+    name = models.CharField(max_length=25) 
     email=models.EmailField(max_length=25)
     phonenumber=models.CharField( max_length=12)
     decrption=models.TextField()
